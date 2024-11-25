09bcfc1f472d660e088ed6855cdd03d9ae461508
Ruhannn <Ruhanrouf17@gmail.com>
2024-11-25 11:48:51 +0600

    added npm publish

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { sharedDir } from './arguments/shareDir';
 import router from './routes';
 import { noCacheMiddleware } from './middleware';
+import path from 'path';
 
 
 const app = express();
@@ -10,9 +11,9 @@ const app = express();
 
 app.use(noCacheMiddleware);
 app.set('view engine', 'ejs');
-app.set('views', './src/views');
+app.set('views', path.join(__dirname, '..', 'src', 'views'));
 app.set('view cache', false);
-app.use(express.static('./public'))
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(express.static(sharedDir));
 
